b10c497813343de9ac33d599796cdff0df40a3ca
Shadman Ahmed <shadman.ahmed098@gmail.com>
2022-02-01 15:35:03 +0000

    provision.sh updated to automatically install dependencies

--- sync/provision.sh
+++ sync/provision.sh
@@ -4,3 +4,13 @@
 sudo apt update -y && sudo apt upgrade -y
 # install nginx
 sudo apt install nginx -y
+# install python-software-properties
+sudo apt install python-software-properties
+# install nodejs v6.x
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+# install nodejs
+sudo apt-get install -y nodejs
+# install pm2 with global flag
+sudo npm install pm2 -g
+# cd into app folder and npm install and start the server
+cd /home/vagrant/app/app && npm install
