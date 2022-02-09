afc5d7b444432c2942c95a776f80ea4c45cfc093
Shadman Ahmed <shadman.ahmed098@gmail.com>
2022-02-09 18:01:47 +0000

    Script

--- AutoscaingGroup-User-Data.sh
+++ AutoscaingGroup-User-Data.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 sudo apt update -y && sudo apt upgrade -y
-echo "export DB_HOST='mongodb://shaddb.duckdns.org:27017/posts'" >> /root/.bashrc
-source /root/.bashrc 
-cd /home/ubuntu/sync/app && screen -d -m npm start
+sudo su ubuntu && echo "cd /home/ubuntu/sync/app && screen -d -m npm start" >> /home/ubuntu/.bashrc && source /home/ubuntu/.bashrc && sudo su && sudo su ubuntu
